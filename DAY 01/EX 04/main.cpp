0873bba5d62153803e8c8791a8b263f16ccc63ce
Saad Gmira <sgmira@e2r5p8.1337.ma>
2023-01-13 16:56:00 +0100

    ex 04

--- DAY 01/EX 04/main.cpp
+++ DAY 01/EX 04/main.cpp
@@ -6,7 +6,7 @@
 /*   By: sgmira <sgmira@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/12 21:23:29 by sgmira            #+#    #+#             */
-/*   Updated: 2023/01/12 22:46:04 by sgmira           ###   ########.fr       */
+/*   Updated: 2023/01/13 16:53:24 by sgmira           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,25 +17,35 @@
 
 int main(int ac, char **av)
 {
-    (void) ac;
-    if(ac > 1)
+    if(ac > 3)
     {
         std::string filename = av[1];
-        // std::string s1;
-        // std::string s2;
+        std::string s1 = av[2];
+        std::string s2 = av[3];
         std::ifstream file(filename);
 
         if(!file) {
             std::cout << "Failed to open file: " << filename << std::endl;
             return 1;
         }
+        std::ofstream outfile(filename + ".replace");
 
         std::string line;
-        while (std::getline(file, line)) {
-            std::cout << line << std::endl;
+        while (std::getline(file, line)) 
+        {
+            size_t pos = 0;
+            while ((pos = line.find(s1, pos)) != std::string::npos)
+            {
+                line.erase(pos, s1.length());
+                line.insert(pos, s2);
+                pos += s2.length();
+            }
+            outfile << line << std::endl;
         }
-
         file.close();
+        outfile.close();
     }
+    else 
+        std::cout << "Not enough arguments" << std::endl;
     return 0;
 }
